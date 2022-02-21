d406f4be46ca21df82d13a3f540ad8466b59c9a5
YichaoAdvam <74939734+YichaoAdvam@users.noreply.github.com>
2022-02-21 18:42:16 +1030

    Update mobile page with functions (#2)
    * added functions
    
    * added get method and updated functions.

--- mobile_page/script.js
+++ mobile_page/script.js
@@ -7,8 +7,12 @@ $(document).ready(function(){
   const demosSection = document.getElementById('demos');
   var btnResult = document.getElementById("btn-check");
   var btnRetake = document.getElementById("btn-retake");
+  var progressDiv = document.getElementById("progressDiv");
+  var progressBar = document.getElementById("progress");
   var snapshot = null;
   var result = document.getElementById("result");
+  var resultDiv = document.getElementById("resultDiv");
+  var inputCardNo = document.getElementById("cardNo");
   var captureImgDiv = document.getElementById("captureImg");
   var edgeDetector = new edgeDetector();
   const threshold = 26;
@@ -44,6 +48,7 @@ $(document).ready(function(){
     // Activate the webcam stream.
     navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
       console.log(stream);
+      this.vidoStream = stream
       video.srcObject = stream;
       video.addEventListener('loadeddata', predictWebcam);
 
@@ -64,6 +69,7 @@ $(document).ready(function(){
       var img = new Image();
 
       ctx.drawImage(video, 0, 0, capture.width, capture.height);
+      //ctx.drawImage(video, 0, -300, capture.width, capture.height);
       img.id = "snapshot";
       img.src = capture.toDataURL("image/png");
       img.width = 640;
@@ -85,21 +91,27 @@ $(document).ready(function(){
     console.log("btnResult clicked");
     initEdgeDetector();
     var rawData = document.getElementById('layer');
+    //var rawData = document.getElementById('rawData');
     var srcData = rawData.toDataURL("image/png");
-    Tesseract.recognize(
-      //snapshot.src,
-      srcData,
-      'eng+por',
-      {
-        logger: m => console.log(m),
-        'psm': 8
-      }
-    ).then(({ data: { text } }) => {
+
+    var worker = Tesseract.createWorker({
+      logger: m => updateProgress(m),
+    });
+
+    const lang = 'eng+por';
+    (async () => {
+      await worker.load();
+      await worker.loadLanguage(lang);
+      await worker.initialize(lang);
+      const { data: { text } } = await worker.recognize(srcData,{
+        psm: 6,
+        init_oem: Tesseract.OEM.TESSERACT_LSTM_COMBINED,
+        tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+      });
       console.log(text);
-      var p = document.createElement('p');
-      p.innerHTML = text;
-      result.appendChild(p);
-    })
+      setValueToInputField(text);
+      await worker.terminate();
+    })();
   }
 
   function retakePhoto () {
@@ -108,8 +120,44 @@ $(document).ready(function(){
     btnResult.classList.add("hidden");
     btnRetake.classList.add("hidden");
     captureImgDiv.innerHTML = "";
-    result.innerHTML = "";
+    resultDiv.classList.add("hidden");
+    inputCardNo.value = null;
   }
+
+  function updateProgress(message) {
+
+    if (message.status == "recognizing text") {
+      console.log(message.progress * 100);
+      if (message.progress < 1) {
+        progressDiv.classList.remove("hidden");
+        progressBar.value = message.progress * 100;
+      } else {
+        progressDiv.classList.add("hidden");
+        progressBar.value = 0;
+      }
+    }
+  }
+
+
+  function getCardNumberLine(text) {
+    var textArray = text.split(/\r\n|\n\r|\n|\r/);
+    var maxlen = 0;
+    var maxindex = 0;
+    for (i = 0; i < textArray.length; i++) {
+      if (textArray[i].length > maxlen) {
+        maxlen = textArray[i].length;
+        maxindex = i;
+      }
+    }
+    return textArray[maxindex];
+  }
+
+  function setValueToInputField (value) {
+    var resultValue = getCardNumberLine(value);
+    inputCardNo.value = resultValue.replace(/\D/g, '');
+    resultDiv.classList.remove("hidden");
+  }
+
 
   function initEdgeDetector () {
     edgeDetector.imgElement = document.getElementById('snapshot');
@@ -241,4 +289,24 @@ $(document).ready(function(){
       this.rawctx.beginPath();
     };
   }
+
+  $('#btn-send').on('click', function(){
+    var data = $('#cardNo').val().replace(/ /g,'');
+    var url = $(this).attr('data-url') + data;
+
+    $.get( url, function() {
+      clearAllandThumbUp();
+      alert("Success!");
+    });
+    //clearAllandThumbUp();
+  });
+
+  function clearAllandThumbUp() {
+    vidoStream.getVideoTracks()[0].stop();
+    var mainDiv = document.getElementById('mainDiv');
+    var thumbUp = document.getElementById('thumbUp');
+    mainDiv.innerHTML = '';
+    thumbUp.classList.remove("hidden");
+  }
+
 });
